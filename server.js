f513dc5b5a9cd6fb42739eeff0b60874c0090a40
Md. Mostafizur Rahman <shanto946@gmail.com>
2017-11-23 15:47:49 +0600

    commit at last 3.47pm 23.11.20017

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 var express=require('express')
 var app=express()
+var bodyParser = require('body-parser')
+var mqtt=require('mqtt')
+
+var client  = mqtt.connect('mqtt://iot.eclipse.org')
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 //database connection 
 var mongojs=require('mongojs')
@@ -15,7 +26,7 @@ app.get('/', function (req,res) {
     })
  })
 
- //app.get('/a',(req,res)=>(db.testData.find()=res.send(docs))));
+ //app.get('/a',(res,req)=>((err,docs)=>res.send(docs)))
  
 
 
@@ -25,6 +36,35 @@ app.get('/temp',(req,res)=>res.send('temparature data here'));
 app.get('/light',(req,res)=>res.send('light data here'));
 //geting humidity data
 app.get('/humidity',(req,res)=>res.send('humidity data here'));
+
+//posting data into database
+app.post('/post',function(req,res) {
+    db.testData.save(req.body);
+    console.log('post here');
+    res.json(req.body);
+    
+});
+
+//mqtt server to mqtt lence code
+
+client.on('connect', function () {
+    client.subscribe('ssdg12')
+    client.publish('ssdg12', 'Hello mqtt')
+  })
+
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    var mes=message.toString();
+    var j={"message":mes};
+    db.testData.save(j)
+    console.log(mes);
+    console.log(j);
+  })
+  
+
+
+
+
 
 console.log('listening')
 app.listen(3000)
